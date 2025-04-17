0ce88ba55c5e15b0724cfa94b889ffb4d8a61baf
Vladimir <kremench@gmail.com>
2025-04-17 09:55:12 +0300

    fix localtorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FeedBack from "./feedback/Feedback";
 import Hero from "./hero/Hero";
 import Navigation from "./navigation/Navigation";
 
 export default function App() {
-  const [feedBackCounts, setfeedBackCounts] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
   const [isView, setIsView] = useState(false);
   const [btnView, setBtnView] = useState(false);
+  const [feedBackCounts, setfeedBackCounts] = useState(() => {
+    const saveFeedBack = window.localStorage.getItem("feed-back-data");
+    if (!saveFeedBack) {
+      return {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+    }
+    return JSON.parse(saveFeedBack);
+  });
+  useEffect(() => {
+    window.localStorage.setItem(
+      "feed-back-data",
+      JSON.stringify(feedBackCounts)
+    );
+  }, [feedBackCounts]);
 
   const handleBtnClick = (feedbackType) => {
     if (feedbackType === "reset") {
